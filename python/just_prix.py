7d7ee49b15fe0fdad7c22e1a00b1defd5bc23c55
alessdenys <alessandro.denys1999@gmail.com>
2020-03-18 16:59:16 +0100

    python

---
+++ python/just_prix.py
@@ -0,0 +1,16 @@
+
+# from random import randrange
+# random = randrange(101)
+# win = False
+# while win == False:
+#         guess = input("entrez un nombre : ")
+#         guess = int(guess)
+#         if guess == random:
+#             print("you win")
+#             win = True
+#         elif guess > random:
+#             print("plus petit")
+#         elif guess < random:
+#             print("plus grand")
+#     except valueError:
+#         print("erreur, veuillez entrer un nombre")
